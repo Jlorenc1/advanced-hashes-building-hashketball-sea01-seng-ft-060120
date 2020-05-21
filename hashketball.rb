27be8805110c90b85cee54699094936f47ea0872
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-21 17:19:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,7 +13,8 @@ def game_hash {
       assists: 12,
       steals: 3,
       blockk: 1,
-      slam_dunks: 1 },
+      slam_dunks: 1 
+    },
     { player_name: "Reggie Evans"
       number: 30,
       shoe: 14,
