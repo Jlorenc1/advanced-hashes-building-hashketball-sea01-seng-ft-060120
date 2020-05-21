b0470cb25455fde2e77f8d55d07148dac65902e8
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-21 17:21:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,10 @@
-def game_hash {
+def game_hash 
+{
   home: {
     team_name: "Brooklyn Nets",
     colors: ["Black", "White"],
     players: [
-      {
-        player_name: "Alan Anderson",
+      { player_name: "Alan Anderson",
         number: 0,
         shoe: 16,
         points: 22,
