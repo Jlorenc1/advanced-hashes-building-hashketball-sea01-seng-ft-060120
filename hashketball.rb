17f2f42895744f5c589cde79936767e873d72bed
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-21 16:14:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,10 +10,10 @@ def game_hash {
       :shoe => 16,
       :points => 22,
       :rebounds => 12,
-      :assists =>12,
-      :steals =>
-      :blocks =>
-      :slam_dunks => },
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1 },
     { :player_name =>
       :number =>
       :shoe =>
